ea88b844ef231887c444357ee985ab5159be592a
Chu Van Nam <11202660@st.neu.edu.vn>
2025-10-02 00:10:56 +0700

    feat: give example of ThreadLocal in Spring

---
+++ src/main/java/com/test/practiceProject/Components/UserContext.java
@@ -0,0 +1,24 @@
+package com.test.practiceProject.Components;
+
+/**
+ * @Project: practiceProject
+ * @Package: com.test.practiceProject.Components  *
+ * @Author: ChuVanNam
+ * @Date: 10/2/2025
+ * @Time: 12:05 AM
+ */
+
+public class UserContext {
+	private static final java.lang.ThreadLocal<String> currentUser = new java.lang.ThreadLocal<>();
+
+	public static void setUser(String user) {
+		currentUser.set(user);
+	}
+
+	public static String getUser() {
+		return currentUser.get();
+	}
+
+	// ❌ Nếu quên remove, data sẽ dính lại trên thread trong pool
+	 public static void clear() { currentUser.remove(); }
+}
